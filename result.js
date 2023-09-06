693588d6f7cca5f6a880c5eb32c8cfaab20fe6fa
bgmun01 <ansqudrms01@gmail.com>
2023-09-06 20:45:29 +0900

    bgmupdate2100

--- result.js
+++ result.js
@@ -7,14 +7,14 @@ const totalScore = parseInt(urlParams.get('score')); // Get the total score from
 const resultElement = document.getElementById('result');
 const scoreElement = document.getElementById('score');
 
-// Display the appropriate message based on the score type
+// Display the appropriate message based on the score type for Investment Type
 if (scoreType === 'stable') {
     resultElement.textContent = '안정형';
 } else if (scoreType === 'adventurous') {
-    resultElement.textContent = '모험형';
+    resultElement.textContent = '위험형';
 } else {
     resultElement.textContent = '';
 }
 
-// Display the total score
-scoreElement.textContent = `총점수 : ${totalScore}`;
+// Display the total score for Investment Score
+scoreElement.textContent = `${totalScore} 점`;
